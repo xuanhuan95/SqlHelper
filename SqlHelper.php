3b52d8ee050dbb135ed8f780a53d23adb01bd171
Huan <nguyenxuanhuan.95@gmail.com>
2016-10-17 18:51:00 +0700

    Complete update method

--- SqlHelper.php
+++ SqlHelper.php
@@ -19,11 +19,44 @@ namespace Library;
 */
 class SqlHelper extends \System\Model
 {
-	protected $table = '';
 
-	public function insert($data='')
+	/**
+	* @var String
+	*/
+	protected $tablePrimary = '';
+
+	/**
+	 * @param string $table
+	 */
+	public function checkTableNameNotNull($table = '')
 	{
-		if( $this->db->insert_row($data)->into($this->table)->query() ){
+		$tableTemp = '';
+
+		// check table name NULL
+		// throw Exception
+		if ($this->tablePrimary == '' && $table == '') {
+			throw new Exception('Require table name!');
+		}
+
+		// set value for var tableTemp
+		if ( $table != '' ) {
+			$tableTemp = $table;
+		}else{
+			$tableTemp = $this->tablePrimary;
+		}
+
+		return $tableTemp;
+	}
+
+	/**
+	* @param Array  $data
+	* @param String $table
+	*/
+	public function addItem($data = array(), $table = '')
+	{
+		$tableTemp = $this->checkTableNameNotNull($table);
+
+		if( $this->db->insert_row($data)->into($tableTemp)->query() ){
 
 		 	return $this->db->last_insert_id();
 
@@ -32,12 +65,44 @@ class SqlHelper extends \System\Model
 		return false;
 	}
 
-	public function update($data = '', $condition = '')
+	/**
+	 * @param Array $listItem
+	 */
+	public function addItems( $listItem = array() , $table = '' )
 	{
+		$tableTemp = $this->checkTableNameNotNull($table);
 
+		foreach ($listItem as $index => $item) {
+			$this->db->insert_row($data)->into($tableTemp)->query()
+		}
 	}
 
-	public function delete($value = '', $condition = '')
+	/**
+	 * @param Array  		$data
+	 * @param Array,String  $condition 'id = abc' or array( array('key'=>key,'operator'=>operator,'value'=>value) )
+	 * @param String 		$table
+	 */
+	public function update( $data = array(), $condition = '', $table = '')
+	{
+		$tableTemp = $this->checkTableNameNotNull($table);
+
+
+		$result = $this->db->update($data)->from($tableTemp);
+
+		if ( !is_array($condition) ) {
+			$result->where($condition);
+		}else{
+
+			foreach ($condition as $key => $con) {
+				$result->where($con['key'], $con['operator'], $con['value']);
+			}
+
+		}
+		$result->query();
+		return $result;
+	}
+
+	public function delete($value = '', $condition = '',$table = '')
 	{
 		# code...
 	}
@@ -56,4 +121,6 @@ class SqlHelper extends \System\Model
 	{
 		# code...
 	}
+
+
 }
