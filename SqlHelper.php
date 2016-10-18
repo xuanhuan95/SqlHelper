65476acbe8d9db583f852b7808a0abde8ec05c34
Huan <nguyenxuanhuan.95@gmail.com>
2016-10-18 21:14:59 +0700

    Complete getItems method

--- SqlHelper.php
+++ SqlHelper.php
@@ -15,7 +15,7 @@
 namespace Library;
 
 /**
-*
+* Simple Class Support Sql query for MaskPHP Frame work
 */
 class SqlHelper extends \System\Model
 {
@@ -23,19 +23,19 @@ class SqlHelper extends \System\Model
 	/**
 	* @var String
 	*/
-	protected $tablePrimary = '';
+	public $tablePrimary = '';
 
 	/**
 	 * @param string $table
 	 */
-	public function checkTableNameNotNull($table = '')
+	private function checkTableNameNotNull($table = '')
 	{
 		$tableTemp = '';
 
 		// check table name NULL
 		// throw Exception
 		if ($this->tablePrimary == '' && $table == '') {
-			throw new Exception('Require table name!');
+			throw new \Exception('Require table name!');
 		}
 
 		// set value for var tableTemp
@@ -46,6 +46,28 @@ class SqlHelper extends \System\Model
 		}
 
 		return $tableTemp;
+	}
+
+	/**
+	 * @param Object 		$obj
+	 * @param Array,String  $condition
+	 */
+	public function checkCondition( $obj = '', $condition = '')
+	{
+		if ( !is_object($obj) ) {
+			throw new \Exception('Type of first param must be Object!');
+		}
+
+		if ( is_string($condition) ) {
+			$obj = $obj->where($condition);
+		}else{
+
+			foreach ($condition as $key => $con) {
+				$obj = $obj->where($con['key'], $con['operator'], $con['value']);
+			}
+		}
+
+		return $obj;
 	}
 
 	/**
@@ -66,15 +88,20 @@ class SqlHelper extends \System\Model
 	}
 
 	/**
-	 * @param Array $listItem
+	 * @param Array  $listItem
+	 * @param String $table
 	 */
 	public function addItems( $listItem = array() , $table = '' )
 	{
 		$tableTemp = $this->checkTableNameNotNull($table);
 
 		foreach ($listItem as $index => $item) {
-			$this->db->insert_row($data)->into($tableTemp)->query()
+
+			$this->db->insert_row($item)->into($tableTemp)->query();
+
 		}
+
+		return true;
 	}
 
 	/**
@@ -86,41 +113,112 @@ class SqlHelper extends \System\Model
 	{
 		$tableTemp = $this->checkTableNameNotNull($table);
 
-
 		$result = $this->db->update($data)->from($tableTemp);
 
-		if ( !is_array($condition) ) {
-			$result->where($condition);
-		}else{
+		$result = $this->checkCondition($result,$condition);
 
-			foreach ($condition as $key => $con) {
-				$result->where($con['key'], $con['operator'], $con['value']);
-			}
+		$result = $result->query();
 
-		}
-		$result->query();
 		return $result;
 	}
 
-	public function delete($value = '', $condition = '',$table = '')
+	/**
+	 * @param Array,String $condition
+	 * @param String 	   $table
+	 */
+	public function delete($condition = '',$table = '')
 	{
-		# code...
+		$tableTemp = $this->checkTableNameNotNull($table);
+
+		$result = $this->db->delete()->from($tableTemp);
+
+		$result = $this->checkCondition($result,$condition);
+
+		$result = $result->query();
+
+		return $result;
+
 	}
 
-	public function getItem($value='', $condition = '')
+	/**
+	 * @param Array,String  $condition
+	 * @param String 		$table
+	 * @param String 		$field  Example: 'name,address' or NULL
+	 */
+	public function getItem($condition = '', $table = '',$field = '')
 	{
-		# code...
+		$tableTemp = $this->checkTableNameNotNull($table);
+
+		if ($field) {
+
+			$item = $this->db->select($field);
+
+		}else{
+
+			$item = $this->db->select();
+
+		}
+
+		$item = $item->from($tableTemp);
+
+		$item = $this->checkCondition($item,$condition);
+
+		$item = $item->fetch_row();
+
+		return $item;
 	}
 
-	public function getItems($value='', $condition = '')
+	//asc desc
+	/**
+	 * @param Array,String  $condition
+	 * @param String 		$table
+	 * @param String 		$field  Example: 'name,address' or NULL
+	 * @param Array         $sort = array( 'field'=> ,'type'=> ); true: DESC, false : ASC
+	 * @param Int           $offset
+	 * @param Int           $limit
+	 */
+	public function getItems( $condition = '', $table = '', $field = '', $sort = array(), $offset = 0,$limit = 50 )
 	{
-		# code...
+		$tableTemp = $this->checkTableNameNotNull($table);
+
+		if ($field) {
+
+			$items = $this->db->select($field);
+
+		}else{
+
+			$items = $this->db->select();
+
+		}
+
+		$items = $items->from($tableTemp);
+
+		$items = $this->checkCondition($items,$condition);
+
+		if ( is_array($sort) ) {
+
+			if ( !array_key_exists('field',$sort) ) {
+				throw new Exception("Array require field key and value not NULL");
+			}
+			$sortType = isset($sort['type']) ? $sort['type'] : false;
+
+			$items = $items->order_by($sort['field'],$sortType);
+		}
+
+		$items = $items->limit($limit,$offset)->fetch();
+
+		return $items;
 	}
 
-	public function formatData($value = '', $condition = '')
+	/**
+	 * @param Array $data
+	 * @param Array $format
+	 */
+	public function formatData($data = '', $format = '')
 	{
 		# code...
 	}
 
 
 }
+
